4163237baa27d107044d029de04040262aaa168f
Victoria Rouser <vic86rous@gmail.com>
2022-03-25 12:09:13 -0500

    finishing touching

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ window.onscroll = () => {
 
 }
 
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
